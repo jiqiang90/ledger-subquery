f473bc3947e984b5fb7c6b6614be6086caa67ea2
Jiri Vestfal <jiri.vestfal@fetch.ai>
2023-03-01 11:32:58 +0000

    refactor: Code simplification

--- genesis-processing/processing/accounts.py
+++ genesis-processing/processing/accounts.py
@@ -28,14 +28,13 @@ class AccountsManager:
 
     def process_genesis(self, genesis_data: dict, chain_id: str):
         accounts_data = self._get_account_data(genesis_data)
-        db_accounts = self.table_manager.select_query([ID])
+        db_accounts = set(self.table_manager.select_query([ID]))
 
-        genesis_accounts_filtered = self._filter_genesis_accounts(
-            accounts_data, db_accounts
-        )
         with self.table_manager.db_copy() as copy:
-            for account in genesis_accounts_filtered:
-                copy.write_row((self._get_account_address(account), chain_id))
+            for account in accounts_data:
+                account_address = self._get_account_address(account)
+                if account_address not in db_accounts:
+                    copy.write_row((account_address, chain_id))
 
     @classmethod
     def _get_account_data(cls, genesis_data: dict) -> List[dict]:
@@ -44,24 +43,3 @@ class AccountsManager:
     @classmethod
     def _get_account_address(cls, account: dict) -> str:
         return str(account["address"])
-
-    @classmethod
-    def _filter_genesis_accounts(
-        cls, accounts_data: List[dict], db_accounts: List[str]
-    ) -> List[dict]:
-        """
-        Filter out genesis_accounts IDs from accounts_data
-
-        :param accounts_data: Account data to be filtered
-        :return: Copy of accounts_data with removed accounts from genesis_accounts filter
-        """
-        genesis_accounts = [cls._get_account_address(x) for x in accounts_data]
-
-        matches = [
-            match for match in set(db_accounts) & set(genesis_accounts)
-        ]  # list already indexed accounts
-        genesis_accounts_filtered = filter(
-            lambda account: cls._get_account_address(account) not in matches,
-            accounts_data,
-        )
-        return list(genesis_accounts_filtered)
